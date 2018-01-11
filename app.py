2e361e85650321398dc40bc6c9e950bae8fbfd73
jabirvpmji <jabirvpmji@gmail.com>
2018-01-11 11:37:28 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template('hello.html')
+@app.route("/test")
+def test():
+    return render_template('test.html')
